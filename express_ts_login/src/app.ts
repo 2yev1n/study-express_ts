5f5e6012193740125ff9e2e91e9cc8ba1874ea19
yebin <yebin5127@gmail.com>
2022-04-07 20:19:08 +0900

    CREATE ts_login main

---
+++ express_ts_login/src/app.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from "cors";
+import morgan from "morgan"
+import * as dotenv from "dotenv";
+import path from 'path';
+import { User } from './models/user';
+
+const app = express();
+const PORT = process.env.PORT||3000;
+
+dotenv.config({ path: path.join(__dirname, "../env") });
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("jwt-secret", process.env.JWT_KEY);
+
+app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
+    res.send('welcome!');
+});
+
+User.sync({ force: false })
+  .then(() => {
+    console.log("database 연결 & table 생성 완료");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(PORT, "번 포트에서 대기 중");
+});
