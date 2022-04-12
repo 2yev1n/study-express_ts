37924ffe7d5e8585a007a88bf7aa3b3bd5190736
yebin <yebin5127@gmail.com>
2022-04-12 19:04:00 +0900

    UPDATE express_ts_login index route

--- express_ts_login/src/routes/index.ts
+++ express_ts_login/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import user from "./user";
-
+import mailer from "./mailer";
+ 
 const router = express();
 
 router.use("/user", user);
+router.use("/", mailer);
 
 export default router;
