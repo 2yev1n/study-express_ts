e09d77e24a6d39546e019f1010ea6f9a92efb947
yebin <yebin5127@gmail.com>
2022-04-14 20:27:11 +0900

    CREATE board user model

---
+++ ts_board/src/models/user.ts
@@ -0,0 +1,43 @@
+import { sequelize } from "../config/config";
+import Sequelize, { Model } from "sequelize";
+import { Board } from "./board";
+
+ export class User extends Model{
+    readonly id!: number;
+    name!: string;
+    password!: string;
+    email!: string;
+    accessToken!: string;
+}
+
+User.init(
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: Sequelize.STRING(5),
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        accessToken: {
+            type: Sequelize.STRING,
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        tableName: "Users",
+        modelName: "users",
+        charset: "UTF8",
+        collate: "UTF8_GENERAL_CI"
+    }
+);
+
+User.hasMany(Board, { foreignKey: "writer", sourceKey: "id" });
+Board.belongsTo(User, { foreignKey: "wirter" });
