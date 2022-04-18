0e17b40a8ec4b29d7468ceba7e798fa7313212c0
yebin <yebin5127@gmail.com>
2022-04-18 22:47:12 +0900

    UPDATE ts_board app

--- ts_board/src/app.ts
+++ ts_board/src/app.ts
@@ -1,10 +1,17 @@
-import express from "express";
-import { sequelize } from "./config/config";
+import express, { Request, Response, NextFunction } from "express";
+import  sequelize from "./models";
 import morgan from "morgan";    // 웹 서버에 들어오는 요청을 shell에 출력해주는 패키지
 import * as dotenv from "dotenv";
 import path from "path";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello, world!")
+})
 
 sequelize.sync({ force: false })
   .then(() => {
@@ -12,4 +19,8 @@ sequelize.sync({ force: false })
   })
   .catch((err) => {
     console.error(err);
+  });
+
+  app.listen(PORT, () => {
+      console.log(PORT, "번 포트에서 대기 중");
   });
