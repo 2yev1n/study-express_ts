63615a95ebefdac6f4a00f8172cf39c3b52859e5
yebin <yebin5127@gmail.com>
2022-04-20 22:45:05 +0900

    UPDATE ts_board config

---
+++ ts_board/src/config/config.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+import config from "./database";
+
+export const sequelize = new Sequelize(
+    config.database.database,
+    config.database.username,
+    config.database.password,
+    {
+        host: config.database.host,
+        dialect: "mysql",
+        define: {
+            timestamps: true,
+        },
+        timezone: "+09:00",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 3000,
+            idle: 10000
+        }
+    }
+);
