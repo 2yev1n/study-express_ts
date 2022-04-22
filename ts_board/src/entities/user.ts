a824044dfd453888c2b90031ac48fcc929e78dd7
yebin <yebin5127@gmail.com>
2022-04-22 10:54:29 +0900

    CREATE ts_board user entity

---
+++ ts_board/src/entities/user.ts
@@ -0,0 +1,32 @@
+import {
+    Column, 
+    Entity, 
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    OneToMany
+} from "typeorm";
+import { Board } from "./board";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    password!: string;
+
+    @OneToMany(
+        (type) => Board, 
+        (board) => board.writer,
+        )
+    board!: Board[];
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt!: Date;
+}
