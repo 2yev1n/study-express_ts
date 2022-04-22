4f91c378fc7debf40d04875d8f06f0d7a0915af5
yebin <yebin5127@gmail.com>
2022-04-22 10:54:20 +0900

    CREATE ts_board board entity

---
+++ ts_board/src/entities/board.ts
@@ -0,0 +1,38 @@
+import { text } from "stream/consumers";
+import {
+    Column, 
+    Entity, 
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn
+} from "typeorm";
+
+import { User } from "./user";
+
+@Entity()
+export class Board{
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    title!: string;
+
+    @Column({ type: "text" })
+    content!: string;
+
+    @ManyToOne(
+        () => User,
+        (user) => user.board,
+        { nullable : false }
+    )
+    @JoinColumn({ name: "writer" })
+    writer!: number;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt!: Date;
+}
