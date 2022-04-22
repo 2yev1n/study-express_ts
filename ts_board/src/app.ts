53287ae32921f69603b890ff3f25aff4c25c891b
yebin <yebin5127@gmail.com>
2022-04-22 10:54:05 +0900

    UPDATE ts_board app

--- ts_board/src/app.ts
+++ ts_board/src/app.ts
@@ -1,26 +1,28 @@
 import express, { Request, Response, NextFunction } from "express";
-import  sequelize from "./models";
 import morgan from "morgan";    // 웹 서버에 들어오는 요청을 shell에 출력해주는 패키지
 import * as dotenv from "dotenv";
 import path from "path";
+import { createConnection } from "typeorm";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
 
 dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.send("Hello, world!")
-})
-
-sequelize.sync({ force: false })
+}); 
+createConnection()
   .then(() => {
-    console.log("database 연결 & table 생성 완료");
+    console.log("데이터베이스 연결 성공");
   })
-  .catch((err) => {
-    console.error(err);
-  });
+  .catch((err) => console.error(err));
 
-  app.listen(PORT, () => {
-      console.log(PORT, "번 포트에서 대기 중");
-  });
+app.listen(PORT, () => {
+    console.log(PORT, "번 포트에서 대기 중");
+});
