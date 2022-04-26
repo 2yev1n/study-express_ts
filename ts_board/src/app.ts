2e8cb3224df4d216d4fbc9f262e5f4c53eeb9630
yebin <yebin5127@gmail.com>
2022-04-26 14:11:49 +0900

    UPDATE ts_board app

--- ts_board/src/app.ts
+++ ts_board/src/app.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";    // 웹 서버에 들어오는 요청을 shell에
 import * as dotenv from "dotenv";
 import path from "path";
 import { createConnection } from "typeorm";
+import router from "./routes/index";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,15 +15,19 @@ app.use(express.urlencoded({ extended : false }));
 
 dotenv.config({ path: path.join(__dirname, "../../.env") });
 
+app.use("/", router);
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.send("Hello, world!")
 }); 
-createConnection()
-  .then(() => {
-    console.log("데이터베이스 연결 성공");
-  })
-  .catch((err) => console.error(err));
 
 app.listen(PORT, () => {
-    console.log(PORT, "번 포트에서 대기 중");
+  console.log(PORT, "번 포트에서 대기 중");
+
+  createConnection()
+    .then(() => {
+      console.log("데이터베이스 연결 성공");
+    })
+    .catch((err) => console.error(err));
+
 });
