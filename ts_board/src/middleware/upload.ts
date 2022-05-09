c2a3b4004854fd4dbf58ded7e804bc3f22d6580a
yebin <yebin5127@gmail.com>
2022-05-09 19:57:17 +0900

    CREATE ts_board s3 middleware

---
+++ ts_board/src/middleware/upload.ts
@@ -0,0 +1,20 @@
+import multer from "multer";
+import multerS3 = require("multer-s3");
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+    accessKeyId: process.env.ACCESSKEY,
+    secretAccessKey: process.env.SECREYACCESSKEY,
+    region: process.env.REGION
+});
+
+export const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: "tsboard",
+        acl: 'public-read',
+        key: function(req, file, cb) {
+            cb(null, Date.now() + '.' + file.originalname.split('.').pop());
+        }
+    })
+});
