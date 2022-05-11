7fce0860c6b2b794015174e12b93d88c0baa0f67
yebin <yebin5127@gmail.com>
2022-05-11 23:14:04 +0900

    UPDATE ts_board post route

--- ts_board/src/routes/post.ts
+++ ts_board/src/routes/post.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import * as Post from "../controllers/post";
 import { authMiddleware } from "../middleware/auth.";
+import { upload } from "../middleware/upload";
 
 const router = express();
 
-router.post("/write", authMiddleware, Post.wirtePost);
+router.post("/write", authMiddleware,upload.array('image'),  Post.wirtePost);
 router.get("/mypage", authMiddleware, Post.readMyPost);
 router.get("/:id", Post.readPost);
 router.patch("/:id", authMiddleware, Post.updatePost);
