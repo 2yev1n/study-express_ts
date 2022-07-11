b017f6fef4312b7cd6208e19a3e0d3ebe93c6c0a
yebin <yebin5127@gmail.com>
2022-07-11 15:32:05 +0900

    FIX ts_board settings

--- ts_board/src/app.ts
+++ ts_board/src/app.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";    // 웹 서버에 들어오는 요청을 shell에 출력해주는 패키지
-import * as dotenv from "dotenv";
-import 'dotenv/config';
+import dotenv from "dotenv";
 import path from "path";
-import { createConnection } from "typeorm";
 import router from "./routes/index";
+import { sequelize } from "./config/config";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -27,11 +26,13 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 app.listen(PORT, () => {
   console.log(PORT, "번 포트에서 대기 중");
 
-  createConnection()
+  sequelize.sync({ force : false })
     .then(() => {
       console.log("데이터베이스 연결 성공");
     })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      console.error(err)
+    });
 
 });
 
