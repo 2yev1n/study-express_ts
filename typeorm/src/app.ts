74605306ed1044f691e731e486fa2a58277bea89
yebin <yebin5127@gmail.com>
2022-07-14 22:27:20 +0900

    CREATE typerom app

---
+++ typeorm/src/app.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import router from "./routes/user";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+dotenv.config({ path: path.join(__dirname, "../../env")})
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("jwt-secret", process.env.KEY);
+
+app.set('/', router);
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello, World');
+});
+
+app.listen(PORT, () => {
+    console.log(PORT, "번 포트에서 대기 중");
+
+    createConnection()
+        .then(() => {
+            console.log("데이터베이스 연결 성공");
+        })
+        .catch((err) => console.error(err))
+
+});
