c3bbbe6475bc5bbf0395d316000620e74bc599f0
yebin <yebin5127@gmail.com>
2022-07-19 17:09:01 +0900

    UPDATE typeorm user route

--- typeorm/src/routes/user.ts
+++ typeorm/src/routes/user.ts
@@ -3,6 +3,7 @@ import * as controller from "../controllers/user";
 
 const router = express();
 
-router.post("signup", controller.signUp);
+router.post("/signup", controller.signUp);
+router.post("/login", controller.login);
 
 export default router;
