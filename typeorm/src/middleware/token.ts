97035c1950e065d49234c6059157157acbc4b9bb
yebin <yebin5127@gmail.com>
2022-07-21 15:04:38 +0900

    FIX typeorm token middleware

--- typeorm/src/middleware/token.ts
+++ typeorm/src/middleware/token.ts
@@ -6,7 +6,8 @@ export const authMiddleware = (
     res: Response,
     next: NextFunction
 ) => {
-    const token = req.header["access-token"]
+    const token: any = req.headers["access-token"];
+    
     if(!token) return res.status(401).json({
         message: "로그인 되어 있지 않음"
     });
